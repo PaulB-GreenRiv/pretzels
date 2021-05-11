3df1517baf6eb965944c802aeb75d6c1a1fb2b49
Paul Butler <pbutler10@mail.greenriver.edu>
2021-05-11 19:04:36 -0400

    Added fields to the form

--- index.php
+++ index.php
@@ -17,13 +17,28 @@ $f3 = Base::instance();
 $f3->route('GET /', function (){
     // display the home page
     $view = new Template();
-    echo $view->render('views/info.html');
+    echo $view->render('views/home.html');
 });
 
 $f3->route('GET|POST /order', function (){
     // display the home page
     $view = new Template();
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        header('location: summary');
+    }
     echo $view->render('views/orderPage.html');
+});
+
+$f3->route('GET|POST /summary', function (){
+    // display the home page
+    $view = new Template();
+    echo $view->render('views/orderSummary.html');
+});
+
+$f3->route('GET|POST /user', function (){
+    // display the home page
+    $view = new Template();
+    echo $view->render('views/userProfile.html');
 });
 
 // run Fat-Free
