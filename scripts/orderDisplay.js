58d1c2d6f19dd71293237e7ac22a3cf3e80a5b4c
Yurii Zhuk <iamyuriizhuk@gmail.com>
2021-05-26 02:23:40 -0400

    Add JS code to display only selected pretzels type

---
+++ scripts/orderDisplay.js
@@ -0,0 +1,9 @@
+/**
+ * This function is used to show and hide appropriate content on order page when Pretzel Type are selected.
+ * @param selector is the current input field. Used word this to refer to current input (onclick="showContent(this)")
+ */
+function showContent(selector)
+{
+    document.getElementById("stuffed_div").style.display = selector.value === "Stuffed" ? 'block' : 'none';
+    document.getElementById("bites_div").style.display = selector.value === "Bitesize" ? 'block' : 'none';
+}
